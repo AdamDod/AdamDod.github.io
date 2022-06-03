1d0034e33265d3752fbbd528b4a30cd0e431a394
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2022-06-03 23:41:46 +1000

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { EmploymentComponent } from './employment/employment.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:AboutMeComponent},
+  {path: 'about-me', component:AboutMeComponent},
+  {path: 'projects', component:ProjectsComponent},
+  {path: 'employment', component:EmploymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MaterialModule} from '../material.module';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EmploymentComponent } from './employment/employment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    EmploymentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

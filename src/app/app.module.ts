a783b10f63f6f2c9b13447d6cc073a905f7bad4a
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2022-07-08 23:44:20 +1000

    remade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import {MaterialModule} from '../material.module';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { EmploymentComponent } from './employment/employment.component';
-import { HomeComponent } from './home/home.component';
+
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent,
-    ProjectsComponent,
-    EmploymentComponent,
-    HomeComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
     MaterialModule,
